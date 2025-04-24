5f4487536db926114f083a183b1263ef17187fe6
daoquy123 <thachdau0203@gmail.com>
2025-04-24 14:58:29 +0700

    Upload full project

---
+++ trangtraiheo_backend/src/main/java/com/trangtraiheo/backend/TrangtraiheoApplication.java
@@ -0,0 +1,13 @@
+package com.trangtraiheo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrangtraiheoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrangtraiheoApplication.class, args);
+	}
+
+}
---
+++ trangtraiheo_backend/src/test/java/com/trangtraiheo/backend/TrangtraiheoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trangtraiheo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrangtraiheoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
